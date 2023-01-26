78603ffe774f65aca5c8a86dfdfc2367914f359e
RealSput (totally sputnix) (rea <117580169+RealSput@users.noreply.github.com>
2023-01-26 17:18:42 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const { Command } = require('commander');
+const vyst = require('./vyst');
+const readFile = require('./read_file');
+const fs = require('fs');
+const zlib = require('zlib');
+const program = new Command();
+
+program
+  .name('Vyst')
+  .description('A file format to make Natural Language Processing easier.')
+  .version('1.8.0');
+
+program.command('compile')
+  .description('Compile a Vyst Text file to a Vyst Format file')
+  .argument('<file>', 'file to compile')
+  .argument('<output>', 'output')
+  .action(async (file, output, options) => {
+    let _f = await readFile(file)
+    _f = _f.toString();
+    fs.writeFileSync(output, zlib.deflateSync(vyst.compile(_f)));
+  });
+
+program.command('decompile')
+  .description('Deompile a Vyst Format file to a Vyst Text file')
+  .argument('<file>', 'file to compile')
+  .argument('<output>', 'output')
+  .action(async (file, output, options) => {
+    let read = await readFile(str);
+    console.log(read);
+    fs.writeFileSync(output, zlib.inflateSync(vyst.decompile(read.toString())));
+  });
+program.command('execute')
+  .description('Execute a Vyst Format file')
+  .argument('<file>', 'file to execute')
+  .argument('<str>', 'string to test AI')
+  .action(async (file, str, options) => {
+    let read = await readFile(file);
+    vyst.execute(zlib.inflateSync(read).toString(), str).then(x => console.log(x));
+  });
+
+program.parse();
+
+module.exports = vyst;
